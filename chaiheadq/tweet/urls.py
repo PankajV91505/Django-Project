beb845a6392c637deaee75d6d476c1a4af08aa40
Pankaj Verma <pankajv91505@gmail.com>
2025-03-01 18:22:28 +0530

    push all code

--- chaiheadq/tweet/urls.py
+++ chaiheadq/tweet/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.tweet_list, name='tweet_list'),
+    path('tweet/create/', views.tweet_create, name='tweet_create'),
+    path('tweet/<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
+    path('tweet/<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
+    path('register/', views.register, name='register'),
+    
     
 ]
